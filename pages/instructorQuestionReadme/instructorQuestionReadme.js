edd721a1774dce1dc996a598624acc23875f50d0
doubleapple123 <leonychen123@gmail.com>
2022-03-09 17:21:56 -0800

    update, catch errors

--- pages/instructorQuestionReadme/instructorQuestionReadme.js
+++ pages/instructorQuestionReadme/instructorQuestionReadme.js
@@ -7,15 +7,22 @@ const { encodePath, decodePath } = require('../../lib/uri-util');
 const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
 const logPageView = require('../../middlewares/logPageView')(path.basename(__filename, '.js'));
 const fs = require('fs');
-const marked = require('marked')
+const { marked } = require('marked');
+const logger = require('../../tests/dummyLogger');
 
 router.get('/', function (req, res, next) {
     let filePath = `${res.locals.course.path}/questions/${res.locals.question.directory}/README.md`;
-    let fileData = fs.readFileSync(filePath, "utf8");
 
-    if (fileData !== null){
-        res.locals.readMeData = marked(fileData);
+    try{
+        let fileData = fs.readFileSync(filePath, "utf8");
+        if (fileData !== null){
+            res.locals.readMeData = marked(fileData);
+        }
     }
+    catch(error){
+        logger.error(`Error while reading readme file, with error ${error}, for filepath ${filePath}`);
+        res.locals.readMeData = "error while reading file";
+    }    
 
     res.render(__filename.replace(/\.js$/, '.ejs'), res.locals);
 });
