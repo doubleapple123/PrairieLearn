d9ef88ef07a97d40de996560dff28ab5d4a1e61b
doubleapple123 <leonychen123@gmail.com>
2022-03-22 17:12:23 -0700

    update

--- middlewares/loadReadmeData.js
+++
@@ -1,9 +0,0 @@
-const ERR = require('async-stacktrace');
-
-const logger = require('../lib/logger');
-
-module.exports = function () {
-    return function(req, res, next){
-
-    };
-};
--- pages/instructorQuestionReadme/instructorQuestionReadme.js
+++ pages/instructorQuestionReadme/instructorQuestionReadme.js
@@ -1,17 +1,17 @@
-// const ERR = require('async-stacktrace');
-// const _ = require('lodash');
-// const express = require('express');
-// const router = express.Router();
-// const path = require('path');
-// const { encodePath, decodePath } = require('../../lib/uri-util');
-// const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
-// const logPageView = require('../../middlewares/logPageView')(path.basename(__filename, '.js'));
-// const fs = require('fs');
-// const { marked } = require('marked');
-// const logger = require('../../tests/dummyLogger');
+const ERR = require('async-stacktrace');
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const { encodePath, decodePath } = require('../../lib/uri-util');
+const debug = require('debug')('prairielearn:' + path.basename(__filename, '.js'));
+const logPageView = require('../../middlewares/logPageView')(path.basename(__filename, '.js'));
+const fs = require('fs');
+const { marked } = require('marked');
+const logger = require('../../tests/dummyLogger');
 
-// router.get('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
 
-// });
+});
 
-// module.exports = router;
+module.exports = router;
